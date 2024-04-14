3f06e781139f4b68af4e14a3a0e208b169969d3e
Nvd34 <tarasenkodimadndz@gmail.com>
2024-04-14 22:18:54 +0300

    upd

--- src/redux/contacts/operations.js
+++ src/redux/contacts/operations.js
@@ -1,13 +1,13 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://connections-api.herokuapp.com/';
-
+// axios.defaults.baseURL = 'https://connections-api.herokuapp.com/';
+const baseURL = 'https://connections-api.herokuapp.com/';
 export const fetchContacts = createAsyncThunk(
   'contacts/fetchContacts',
   async (_, thunkAPI) => {
     try {
-      const response = await axios.get('/contacts');
+      const response = await axios.get(baseURL + '/contacts');
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
@@ -19,7 +19,7 @@ export const addContact = createAsyncThunk(
   'contacts/addContact',
   async (newContact, thunkAPI) => {
     try {
-      const response = await axios.post('/contacts', newContact);
+      const response = await axios.post(baseURL + '/contacts', newContact);
       return response.data;
     } catch (error) {
       console.log('error in catch');
@@ -32,7 +32,7 @@ export const deleteContact = createAsyncThunk(
   'contacts/deleteContact',
   async (idContact, thunkAPI) => {
     try {
-      const response = await axios.delete(`/contacts/${idContact}`);
+      const response = await axios.delete(baseURL + `/contacts/${idContact}`);
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
@@ -44,10 +44,13 @@ export const editContact = createAsyncThunk(
   'contacts/editContact',
   async (editContact, thunkAPI) => {
     try {
-      const response = await axios.patch(`/contacts/${editContact.id}`, {
-        name: editContact.name,
-        number: editContact.number,
-      });
+      const response = await axios.patch(
+        baseURL + `/contacts/${editContact.id}`,
+        {
+          name: editContact.name,
+          number: editContact.number,
+        }
+      );
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
